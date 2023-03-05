13f601953608dd3d4d5ab29629c32c6f183a3008
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2023-03-05 21:45:33 +0300

    Testing username

---
+++ spec/requests/usernames_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe "Usernames", type: :request do
+    let(:user) { create(:user, username: nil) }
+
+    before { sign_in user }
+
+    describe "GET new" do
+        it "successful" do
+            get new_username_path
+            expect(response).to have_http_status(:success)
+        end
+    end
+
+    describe "PUT update" do
+        it "updates the username" do
+            expect do 
+                put username_path(user), params: {
+                    user: {
+                        username: "foobar"
+                    }
+                }
+            end.to change { user.reload.username }.from(nil).to("foobar")
+            expect(response).to redirect_to(dashboard_path)
+        end
+    end
+
+end
