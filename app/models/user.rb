ecd932c2fd2e4c3979a5a29caf902d22853fa8c0
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2023-03-05 14:13:01 +0300

    Add uniqueness to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :tweets, dependent: :destroy
+
+  validates :username, uniqueness: { case_sensitive: false }, allow_blank: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { should have_many(:tweets).dependent(:destroy)}
+  it { should have_many(:tweets).dependent(:destroy) }
+  it { should validate_uniqueness_of(:username).case_insensitive.allow_blank }
+
 end
